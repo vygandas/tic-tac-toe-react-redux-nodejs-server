5a77cfc609b56b3de7b30a67b0af5f82e57fb251
vygandas <vygandas.pliasas@gmail.com>
2018-06-27 22:11:24 +0300

    add CORS support; fix messages;

--- src/App.ts
+++ src/App.ts
@@ -16,6 +16,12 @@ class App {
     private mountRoutes (): void {
         const router = express.Router();
 
+        router.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
+
         router.get('/', (req, res) => { res.json(responseObject('Hello Players! Wanna play?', this.gameBoard)); });
 
         router.get('/reset', (req, res) => {
@@ -24,20 +30,12 @@ class App {
         });
 
         router.get('/mark', (req, res) => {
-            if (!this.gameBoard.isGameOver()) {
-                if (this.gameBoard.setMark(parseInt(req.query.x), parseInt(req.query.y))) {
-                    res.json(responseObject(`Cell used [x:${req.query.x};y:${req.query.y}]. Now it's turn for ${this.gameBoard.getWhosTurn()}`, this.gameBoard));
-                } else {
-                    res.json(responseObject(`Cell is occupied [x:${req.query.x};y:${req.query.y}]. Now it's still turn for ${this.gameBoard.getWhosTurn()}`, this.gameBoard));
-                }
+            if (this.gameBoard.setMark(parseInt(req.query.x), parseInt(req.query.y))) {
+                res.json(responseObject(`Cell used [x:${req.query.x};y:${req.query.y}]. Now it's turn for ${this.gameBoard.getWhosTurn()}`, this.gameBoard));
             } else {
-                const msg = this.gameBoard.getWinner() === null ? 'It\'s a tie :|' : `${this.gameBoard.getWinner()} has won!`;
-                res.json(responseObject(`GAME OVER! ${msg}`, this.gameBoard));
+                res.json(responseObject(`Cell is occupied [x:${req.query.x};y:${req.query.y}]. Now it's still turn for ${this.gameBoard.getWhosTurn()}`, this.gameBoard));
             }
-
         });
-
-        router.get('/winner', (req, res) => { res.json(responseObject(this.gameBoard.getWinner(), this.gameBoard)); });
 
         this.express.use('/', router);
     }
--- src/Response/ResponseObject.ts
+++ src/Response/ResponseObject.ts
@@ -1,7 +1,14 @@
 import Board from "../engine/Board";
 
-export const responseObject = (message: string, board: Board) => ({
-    message,
-    gameOver: board.isGameOver(),
-    grid: board.getBoardGrid()
-});
+export const responseObject = (message: string | number | null, board: Board) => {
+    const gameOver = board.isGameOver();
+    let msg = message;
+    if (gameOver) {
+        msg = `GAME OVER! ${board.getWinner()} has won!`;
+    }
+    return {
+        message: msg,
+        gameOver,
+        grid: board.getBoardGrid()
+    };
+};
